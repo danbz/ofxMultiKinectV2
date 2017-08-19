b8855a4fd2f394df2cd433ee91226674d8792b0e
Dan B <dan@buzzo.com>
2017-08-19 17:23:23 +0200

    updated pointcloud example with realworld point color

--- example_pointcloud/src/ofApp.cpp
+++ example_pointcloud/src/ofApp.cpp
@@ -1,12 +1,16 @@
 #include "ofMain.h"
 #include "ofxMultiKinectV2.h"
 
+
+// dan buzzo - added updates to render color on point cloud
 //========================================================================
 class ofApp : public ofBaseApp{
     ofxMultiKinectV2 kinect0;
     
     ofEasyCam ecam;
     ofVboMesh mesh;
+    
+    ofPixels colorImage;
 public:
     
     void setup()
@@ -14,7 +18,7 @@ public:
         ofSetVerticalSync(true);
         ofSetFrameRate(60);
         
-        kinect0.open(false, true, 0);
+        kinect0.open(true, true, 0);
         // Note :
         // Default OpenCL device might not be optimal.
         // e.g. Intel HD Graphics will be chosen instead of GeForce.
@@ -29,25 +33,36 @@ public:
         ecam.setAutoDistance(false);
         ecam.setDistance(200);
         
+        
     }
     
     void update() {
         kinect0.update();
         if (kinect0.isFrameNew()) {
             mesh.clear();
+            
+            colorImage = kinect0.getColorPixelsRef();
+            
             {
-                int step = 2;
+                int step = 1;
                 int h = kinect0.getDepthPixelsRef().getHeight();
                 int w = kinect0.getDepthPixelsRef().getWidth();
+                float cW = colorImage.getWidth();
+                float cH = colorImage.getHeight();
+                //cout << w << " width " << "scale " << cW << "height " << h <<  "scale :" << cH << endl;
                 for(int y = 0; y < h; y += step) {
                     for(int x = 0; x < w; x += step) {
                         float dist = kinect0.getDistanceAt(x, y);
-                        if(dist > 50 && dist < 500) {
+                        if(dist > 50 && dist < 5000) {
                             ofVec3f pt = kinect0.getWorldCoordinateAt(x, y, dist);
                             
                             ofColor c;
+                            int colX = x * 3.75;
+                            int colY = y * 2.55;
+                           c =  colorImage.getColor(colX, colY);
+                           // cout << c << " " << colX << " " << colY << endl;
                             float h = ofMap(dist, 50, 200, 0, 255, true);
-                            c.setHsb(h, 255, 255);
+                            //c.setHsb(h, 255, 255);
                             mesh.addColor(c);
                             mesh.addVertex(pt);
                         }
